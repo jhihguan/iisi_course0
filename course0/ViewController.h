d65569ef26b8044398fe443248961634c01e51e8
Jhihguan <imdiedie@gmail.com>
2013-08-05 13:13:19 +0800

    Initial Commit

---
+++ course0/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  course0
+//
+//  Created by Jhihguan on 13/8/5.
+//  Copyright (c) 2013年 Jhihguan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ course0/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  course0
+//
+//  Created by Jhihguan on 13/8/5.
+//  Copyright (c) 2013年 Jhihguan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
