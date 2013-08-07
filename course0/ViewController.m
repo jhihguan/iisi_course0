989dedc4222afb14b048e940861ae64c0411e7c0
Jhihguan <imdiedie@gmail.com>
2013-08-08 00:12:38 +0800

    finish program

--- course0/ViewController.m
+++ course0/ViewController.m
@@ -7,12 +7,51 @@
 //
 
 #import "ViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIButton *button1;
+@property (weak, nonatomic) IBOutlet UIButton *button2;
+@property (weak, nonatomic) IBOutlet UIButton *button3;
 @end
 
 @implementation ViewController
+- (IBAction)selectedCard:(UIButton *)sender {
+//    sender.selected = !sender.isSelected;
+    if (!sender.isSelected) {
+        sender.layer.borderWidth = 3.0;
+        sender.layer.borderColor = [UIColor colorWithRed:1.0 green:0.2 blue:0.2 alpha:1].CGColor;
+    }else{
+        sender.layer.borderWidth = 0.0;
+    }
+    sender.selected = !sender.isSelected;
+}
+
+- (IBAction)changeButtonText:(UIButton *)sender {
+    if (!sender.isSelected) {
+        if (!_button1.isSelected) {
+            [_button1 setTitle:@"Show1" forState:UIControlStateNormal];
+        }
+        if (!_button2.isSelected) {
+            [_button2 setTitle:@"Show2" forState:UIControlStateNormal];
+        }
+        if (!_button3.isSelected) {
+            [_button3 setTitle:@"Show3" forState:UIControlStateNormal];
+        }
+    }else{
+        if (!_button1.isSelected) {
+            [_button1 setTitle:@"Button1" forState:UIControlStateNormal];
+        }
+        if (!_button2.isSelected) {
+            [_button2 setTitle:@"Button2" forState:UIControlStateNormal];
+        }
+        if (!_button3.isSelected) {
+            [_button3 setTitle:@"Button3" forState:UIControlStateNormal];
+        }
+    }
+    sender.selected = !sender.isSelected;
+    
+}
 
 - (void)viewDidLoad
 {
